3972e0209a571e9674bb815d5fef151c7faa9d76
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2022-12-07 11:31:08 +0900

    Create project and modules.

---
+++ app/README.md
@@ -0,0 +1,7 @@
+デジタル庁デザインシステムAndroidテーマ（非公式）
+=======
+
+公開されているデザインシステムのファイルを参考に、Androidのテーマとして実装する。
+
+* JDD UI Kit Android(Community)
+    * https://www.figma.com/file/cP1qs2cI1SepOOhFRp0s5g/JDD-%7C-JP-%2B-UI-Kit-Android-(Community)?node-id=1%3A139&t=iDYXaXZ1MSdZsbva-0
---
+++ theme-compose/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "jp.go.digital.theme"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "consumer-rules.pro"
+        )
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    val composeUiVersion = "1.2.1"
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.7.0")
+
+    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.compose.ui:ui:$composeUiVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeUiVersion")
+    implementation("androidx.compose.material:material:1.3.1")
+
+    testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeUiVersion")
+
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeUiVersion")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeUiVersion")
+}
